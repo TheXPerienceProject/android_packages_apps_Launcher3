ebc296288f78a21edb8dd8a3ec8fa882e1c463e1
klozz <klozz@vivaldi.net>
2019-09-18 01:19:09 +0000

    NightFall bring
    Change-Id: I48cd323b4217451688e8f17fa7471253e15c836d
    Signed-off-by: klozz <klozz@vivaldi.net>

--- Android.mk
+++ Android.mk
@@ -40,7 +40,7 @@ LOCAL_MODULE := LauncherPluginLib
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 #
-# Build rule for Launcher3 dependencies lib.
+# Build rule for Nightfall dependencies lib.
 #
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
@@ -70,21 +70,21 @@ LOCAL_PROTO_JAVA_OUTPUT_PARAMS := enum_style=java
 
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 21
-LOCAL_MODULE := Launcher3CommonDepsLib
+LOCAL_MODULE := NightfallCommonDepsLib
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MANIFEST_FILE := AndroidManifest-common.xml
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 #
-# Build rule for Launcher3 app.
+# Build rule for Nightfall app.
 #
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    Launcher3CommonDepsLib \
+    NightfallCommonDepsLib \
     SecondaryDisplayLauncherLib
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
@@ -98,25 +98,25 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 21
-LOCAL_PACKAGE_NAME := Launcher3
+LOCAL_PACKAGE_NAME := Nightfall
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.Nightfall
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest-common.xml
 
-LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.launcher3.*
+LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.Nightfall.*
 
 include $(BUILD_PACKAGE)
 
 #
-# Build rule for Launcher3 Go app for Android Go devices.
+# Build rule for Nightfall Go app for Android Go devices.
 #
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
+LOCAL_STATIC_ANDROID_LIBRARIES := NightfallCommonDepsLib
 
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
@@ -129,18 +129,18 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 21
-LOCAL_PACKAGE_NAME := Launcher3Go
+LOCAL_PACKAGE_NAME := NightfallGo
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep Launcher3QuickStepGo
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Nightfall NightfallQuickStep NightfallQuickStepGo
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.Nightfall
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
     $(LOCAL_PATH)/AndroidManifest.xml \
     $(LOCAL_PATH)/AndroidManifest-common.xml
 
 LOCAL_MANIFEST_FILE := go/AndroidManifest.xml
-LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.launcher3.*
+LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.Nightfall.*
 include $(BUILD_PACKAGE)
 
 #
@@ -159,10 +159,10 @@ else
   LOCAL_SDK_VERSION := system_current
   LOCAL_MIN_SDK_VERSION := 26
 endif
-LOCAL_MODULE := Launcher3QuickStepLib
+LOCAL_MODULE := NightfallQuickStepLib
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    Launcher3CommonDepsLib \
+    NightfallCommonDepsLib \
     SecondaryDisplayLauncherLib
 
 LOCAL_SRC_FILES := \
@@ -188,7 +188,7 @@ include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3QuickStepLib
+LOCAL_STATIC_ANDROID_LIBRARIES := NightfallQuickStepLib
 LOCAL_PROGUARD_ENABLED := disabled
 
 ifneq (,$(wildcard frameworks/base))
@@ -197,11 +197,11 @@ else
   LOCAL_SDK_VERSION := system_current
   LOCAL_MIN_SDK_VERSION := 26
 endif
-LOCAL_PACKAGE_NAME := Launcher3QuickStep
+LOCAL_PACKAGE_NAME := NightfallQuickStep
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 XPeriaHome
-LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Nightfall XPeriaHome
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.Nightfall
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/res \
@@ -218,7 +218,7 @@ include $(BUILD_PACKAGE)
 
 
 #
-# Build rule for Launcher3 Go app with quickstep for Android Go devices.
+# Build rule for Nightfall Go app with quickstep for Android Go devices.
 #
 include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
@@ -232,7 +232,7 @@ else
   LOCAL_SDK_VERSION := system_current
   LOCAL_MIN_SDK_VERSION := 26
 endif
-LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
+LOCAL_STATIC_ANDROID_LIBRARIES := NightfallCommonDepsLib
 
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
@@ -248,10 +248,10 @@ LOCAL_RESOURCE_DIR := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PROGUARD_ENABLED := full
 
-LOCAL_PACKAGE_NAME := Launcher3QuickStepGo
+LOCAL_PACKAGE_NAME := NightfallQuickStepGo
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep Launcher3GoIconRecents XPeriaHome
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Nightfall NightfallQuickStep NightfallGoIconRecents XPeriaHome
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
@@ -264,7 +264,7 @@ LOCAL_JACK_COVERAGE_INCLUDE_FILTER := com.android.launcher3.*
 include $(BUILD_PACKAGE)
 
 #
-# Build rule for Launcher3 Go app with quickstep and Go-specific
+# Build rule for Nightfall Go app with quickstep and Go-specific
 # version of recents for Android Go devices.
 #
 include $(CLEAR_VARS)
@@ -279,7 +279,7 @@ else
   LOCAL_SDK_VERSION := system_current
   LOCAL_MIN_SDK_VERSION := 26
 endif
-LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
+LOCAL_STATIC_ANDROID_LIBRARIES := NightfallCommonDepsLib
 
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
@@ -295,10 +295,10 @@ LOCAL_RESOURCE_DIR := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PROGUARD_ENABLED := full
 
-LOCAL_PACKAGE_NAME := Launcher3GoIconRecents
+LOCAL_PACKAGE_NAME := NightfallGoIconRecents
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3Go Launcher3QuickStep
+LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Nightfall NightfallGo NightfallQuickStep
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.launcher3
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := \
