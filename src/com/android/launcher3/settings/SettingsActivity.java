5fc68fa712969cc88caa5e4d9f3b1024436241ee
klozz <klozz@TheXPerienceProject.org>
2021-03-17 04:27:22 +0100

    Revert "NightFallLauncher: Allow changing app suggestion settings."
    This reverts commit 60d7137d5e976788b03c03803a0ce2836ffd2552.
    
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -28,6 +28,7 @@ import static mx.xperience.launcher.OverlayCallbackImpl.KEY_ENABLE_MINUS_ONE;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
