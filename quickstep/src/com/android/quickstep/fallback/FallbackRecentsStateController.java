c33ffe7133a4c77fd7f881c884919d6ee8e2e7e0
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    Launcher3: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    iusmac: adapt to A13.
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    neobuddy89: adapt to A13 QPR2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- quickstep/src/com/android/quickstep/fallback/FallbackRecentsStateController.java
+++ quickstep/src/com/android/quickstep/fallback/FallbackRecentsStateController.java
@@ -54,6 +54,7 @@ import com.android.launcher3.statemanager.StateManager.StateHandler;
 import com.android.launcher3.states.StateAnimationConfig;
 import com.android.quickstep.views.AddDesktopButton;
 import com.android.quickstep.views.ClearAllButton;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.RecentsViewContainer;
 
@@ -158,6 +159,9 @@ public class FallbackRecentsStateController implements StateHandler<RecentsState
         setter.setFloat(mRecentsView, taskViewsFloat.first, isSplitSelectionState(state)
                 ? mRecentsView.getSplitSelectTranslation() : 0, LINEAR);
         setter.setFloat(mRecentsView, taskViewsFloat.second, 0, LINEAR);
+        float memInfoAlpha = state.hasMemInfoView() ? 1 : 0;
+        setter.setFloat(mActivity.getMemInfoView(), MemInfoView.STATE_CTRL_ALPHA,
+                memInfoAlpha, LINEAR);
     }
 
     private Interpolator getOverviewInterpolator(RecentsState toState) {
--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -326,6 +326,10 @@ public class DeviceProfile {
     // DragController
     public int flingToDeleteThresholdVelocity;
 
+    // Meminfo in overview
+    public int memInfoMarginGesturePx;
+    public int memInfoMarginThreeButtonPx;
+
     /** Used only as an alternative to mocking when null values cannot be used. */
     @VisibleForTesting
     public DeviceProfile() {
@@ -828,6 +832,11 @@ public class DeviceProfile {
         } else {
             isLeftRightSplit = isLandscape;
         }
+
+        memInfoMarginGesturePx = res.getDimensionPixelSize(
+                R.dimen.meminfo_bottom_margin_gesture);
+        memInfoMarginThreeButtonPx = res.getDimensionPixelSize(
+                R.dimen.meminfo_bottom_margin_three_button);
 
         // Calculate all of the remaining variables.
         extraSpace = updateAvailableDimensions(context);
