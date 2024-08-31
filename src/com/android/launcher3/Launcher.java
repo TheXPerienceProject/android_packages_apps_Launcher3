08ba1fa69a0449084b4630e78f20fe6398cee973
Pranav Vashi <neobuddy89@gmail.com>
2024-08-31 22:25:39 +0530

    Launcher3: Allow setting transparency for hotseat search bar
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/8244819d65c45f642685d24d58fd1397fe54e32e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -441,6 +441,7 @@ public class Launcher extends StatefulActivity<LauncherState>
                     case Utilities.KEY_SINGLE_PAGE_CENTER:
                     case Utilities.KEY_DRAWER_SEARCH:
 	            case Utilities.KEY_DOCK_MUSIC_SEARCH:
+	            case Utilities.KEY_HOTSEAT_QSB_OPACITY:
                         mNeedsRestart = true;
                         break;
                     default:
