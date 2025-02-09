03e5e3756ebf88e0f8c9f70b025d29ecbc60ce08
klozz <carlosj@klozz.dev>
2025-02-09 01:52:39 -0600

    Revert "Launcher3: Integrate LMOFreeform to system shortcut"
    klzz: reason to revert: is duplicated
    
    This reverts commit 1ef9d2dd7ba31c72dff755a5fe305e5eb89d122f.

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -47,7 +47,6 @@ import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.BUBBLE_SHORTCUT;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
 import static com.android.launcher3.popup.SystemShortcut.FREE_FORM;
-import static com.android.launcher3.popup.SystemShortcut.FLOATING;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PAUSE_APPS;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
@@ -490,7 +489,6 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         shortcuts.add(UNINSTALL);
         shortcuts.add(PAUSE_APPS);
         shortcuts.add(FREE_FORM);
-        shortcuts.add(FLOATING);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
         if (Flags.enablePrivateSpaceInstallShortcut()) {
