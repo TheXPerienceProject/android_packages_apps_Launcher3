6db1467f5353b75ab5f7dfaebadd9d30a4c83306
klozz <carlosj@klozz.dev>
2025-03-03 21:51:57 +0000

    PendingAddWidgetInfo:Prevent NullPointerException in PendingAddWidgetInfo
    Added null check for providerInfo to avoid crash.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/launcher3/widget/PendingAddWidgetInfo.java
+++ src/com/android/launcher3/widget/PendingAddWidgetInfo.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -45,12 +45,21 @@ public class PendingAddWidgetInfo extends PendingAddItemInfo {
 
     public WidgetRecommendationCategory recommendationCategory = null;
 
+    private boolean mCustomWidget;
+
     public PendingAddWidgetInfo(
             LauncherAppWidgetProviderInfo i,
             int container,
             WidgetRecommendationCategory recommendationCategory) {
         this(i, container);
         this.recommendationCategory = recommendationCategory;
+
+        // Null check
+        if (i != null && i.isCustomWidget()) {
+            mCustomWidget = true;
+        } else {
+            mCustomWidget = false;
+        }
     }
 
     public PendingAddWidgetInfo(LauncherAppWidgetProviderInfo i, int container) {
