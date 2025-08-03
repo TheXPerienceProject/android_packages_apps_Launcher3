1f2c189cd6f416741a45846f1cd8c8cb75797cd3
klozz <carlosj@klozz.dev>
2025-08-03 06:41:55 -0600

    enable meminfo by default on clean builds
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1123,6 +1123,6 @@ public final class Utilities {
 
     public static boolean isShowMeminfo(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getBoolean(KEY_RECENTS_MEMINFO, false);
+        return prefs.getBoolean(KEY_RECENTS_MEMINFO, true);
    }
 }
